e437e803da5565709f82b3161dac1ccdf619a31a
Sashkoooo <o.denysenko@hotmail.com>
2024-11-18 15:02:16 +0200

    Add USER_AGENT settings

--- scrape_sites/settings.py
+++ scrape_sites/settings.py
@@ -15,6 +15,12 @@ NEWSPIDER_MODULE = "scrape_sites.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "scrape_sites (+http://www.yourdomain.com)"
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    " AppleWebKit/537.36 (KHTML, like Gecko)"
+    " Chrome/87.0.4280.88 Safari/537.36"
+)
+
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
