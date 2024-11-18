93d4a505e32d980efb2b34dc3305d7300ff2a5b9
Sashkoooo <o.denysenko@hotmail.com>
2024-11-18 15:01:54 +0200

    Add items

--- scrape_sites/items.py
+++ scrape_sites/items.py
@@ -7,6 +7,7 @@ import scrapy
 
 
 class ScrapeSitesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    company = scrapy.Field()
+    description = scrapy.Field()
+    url = scrapy.Field()
