1e3986d4afe600d91fac88c7f9d599131fe68d93
Sashkoooo <o.denysenko@hotmail.com>
2024-12-04 21:09:08 +0200

    Remove VacancyLinksParser class

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
 
+from links_parser.parser import ScrapVacancySite
+# from links_parser.parser import get_all_links
 
 def get_all_links() -> None:
     """Save all pages to corresponding .txt files"""
-    site_to_scrap = ScrapDouSite()
+    site_to_scrap = ScrapVacancySite()
     try:
         site_to_scrap.scrap_pages()
     except Exception as error:
@@ -13,16 +15,15 @@ def get_all_links() -> None:
 
 
 if __name__ == "__main__":
-    # Виконати get_all_links()
+
     get_all_links()
     print("Links gathered successfully.")
 
-    # Запустити Scrapy crawler
-    print("Starting Scrapy crawler...")
-    try:
-        subprocess.run(
-            ["scrapy", "crawl", "dou_spider", "-o", "output.jsonl"], check=True
-        )
-        print("Scrapy crawler completed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error while running Scrapy: {e}")
+    # print("Starting Scrapy crawler...")
+    # try:
+    #     subprocess.run(
+    #         ["scrapy", "crawl", "dou_spider", "-o", "data/dou.jsonl"], check=True
+    #     )
+    #     print("Scrapy crawler completed successfully.")
+    # except subprocess.CalledProcessError as e:
+    #     print(f"Error while running Scrapy: {e}")
