e49895c616eaa715b955b653f51d10300fc05a90
Sashkoooo <o.denysenko@hotmail.com>
2024-12-24 18:22:04 +0200

    implement class for technologies counting

---
+++ data_analysis/techs_counting.py
@@ -0,0 +1,86 @@
+import json
+import re
+import nltk
+import csv
+from nltk.tokenize import word_tokenize
+from pathlib import Path
+from collections import Counter
+from config import technology_groups
+from datetime import datetime
+from diagram import diagram
+
+nltk.download("punkt")
+
+
+class TechnologyCounting:
+    """Class to count technologies in descriptions"""
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+        self.technology_counter = Counter()
+        self.technology_counting()
+
+    @staticmethod
+    def file_path(file_name) -> Path:
+        base_dir = Path.cwd().parent
+        file_path = base_dir / "data" / file_name
+        return file_path.resolve()
+
+    def descriptions(self) -> list[str]:
+        descriptions = []
+        try:
+            with open(
+                    self.file_path(self.file_name), "r", encoding="utf-8"
+            ) as file:
+                for line in file:
+                    data = json.loads(line)
+                    descriptions.append(data.get("description", ""))
+        except FileNotFoundError:
+            print(f"File {self.file_name} is not found.")
+        except json.JSONDecodeError:
+            print(f"JSON file {self.file_name} decoding error.")
+        return descriptions
+
+    @staticmethod
+    def preprocess_text(text: str) -> list[str]:
+        """Text preprocessing"""
+        text = text.lower()
+        text = re.sub(r"[^a-zA-Z0-9\s]", " ", text)
+        tokens = word_tokenize(text)
+        return tokens
+
+    def tokenized_descriptions(self) -> list[list[str]]:
+        tokenized_descriptions = [
+            self.preprocess_text(desc) for desc in self.descriptions()
+        ]
+        return tokenized_descriptions
+
+    def technology_counting(self) -> None:
+        for tokens in self.tokenized_descriptions():
+            unique_technologies = set()
+            for tech, synonyms in technology_groups.items():
+                if any(synonym in tokens for synonym in synonyms):
+                    unique_technologies.add(tech)
+            self.technology_counter.update(unique_technologies)
+
+
+    def save_to_csv(self, output_file: str = None) -> None:
+        if output_file is None:
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+            output_file = f"techs_counting_{timestamp}.csv"
+
+        with open(
+                self.file_path(output_file), "w", newline="", encoding="utf-8"
+        ) as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Technology", "Frequency"])
+            for tech, count in self.technology_counter.items():
+                writer.writerow([tech, count])
+
+        print(f"The results are saved to: {output_file}")
+
+
+if __name__ == "__main__":
+
+    techs = TechnologyCounting("dou.jsonl")
+    techs.save_to_csv()
+    diagram(techs.technology_counter)
