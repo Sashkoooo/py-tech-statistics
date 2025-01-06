9ae82712252be457534ee4246d006525ff076e14
Sashkoooo <o.denysenko@hotmail.com>
2025-01-06 20:42:16 +0200

    Move DouURLs dataclass & get_dou_links method to specific module

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
 import subprocess
 
-from links_parser.parser import ScrapVacancySite
-from dataclasses import dataclass, field
-from typing import Dict
+from data_analysis.techs_handling import techs_handling
+from links_parser.urls_handling import get_dou_links
 
-@dataclass
-class DouURLs:
-    base_url: str = "https://jobs.dou.ua/"
-    pages_urls: Dict[str, str] = field(default_factory=lambda: {
-        "dou_python": "vacancies/?category=Python",
-    })
-
-def get_dou_links() -> None:
-    """Save all page links to corresponding .txt files"""
-    urls = DouURLs()
-    site = ScrapVacancySite()
-    try:
-        pages = site.get_pages(urls.base_url, urls.pages_urls)
-        for page_name, page_url in pages.items():
-            print(f"Processing page: {page_name}")
-            site.open_page(page_url)
-            site.click_more_button(css_selector="div.more-btn a")
-            site.write_links_to_txt(
-                f"{page_name}_links.txt",
-                site.vacancies_links(css_selector="a.vt")
-            )
-    except Exception as error:
-        print(f"Error during scraping: {error}")
-    site.close_browser()
 
 if __name__ == "__main__":
 
@@ -37,9 +12,11 @@ if __name__ == "__main__":
     print("Starting Scrapy crawler...")
     try:
         subprocess.run(
-            "python -m scrapy crawl dou_spider -o data/dou.jsonl",
+            "python -m scrapy crawl dou_spider -o data/source/dou.jsonl",
             check=True
         )
         print("Scrapy crawler completed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error while running Scrapy: {e}")
+
+    techs_handling("dou.jsonl")
