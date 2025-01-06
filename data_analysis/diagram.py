cddb1c6631e0477267a55aa14a0ccea3ee312aab
Sashkoooo <o.denysenko@hotmail.com>
2025-01-06 20:36:44 +0200

    Add sorting technologies & frequency labels

--- data_analysis/diagram.py
+++ data_analysis/diagram.py
@@ -4,28 +4,37 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 def diagram(technology_groups: dict) -> None:
-    # Створення папки diagrams, якщо вона не існує
-    diagrams_dir = Path.cwd().parent / "data" / "diagrams"
+    """
+    Create a bar chart to visualize
+    the frequency of technologies
+    in job descriptions
+    """
+    diagrams_dir = Path.cwd() / "data" / "diagrams"
     diagrams_dir.mkdir(parents=True, exist_ok=True)
 
-    # Поточна дата й час
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    diagram_file = f"technology_frequencies_chart_{current_time}.png"
+    diagram_file = diagrams_dir / f"technology_frequencies_chart_{current_time}.png"
 
-    technologies = list(technology_groups.keys())
-    frequencies = list(technology_groups.values())
+    # Sort technologies by frequency
+    sorted_technology_groups = sorted(
+        technology_groups.items(), key=lambda x: x[1], reverse=True
+    )
+    technologies = [item[0] for item in sorted_technology_groups]
+    frequencies = [item[1] for item in sorted_technology_groups]
 
-    # Створення стовпчикової діаграми
-    plt.figure(figsize=(10, 6))
-    plt.bar(technologies, frequencies, color="skyblue")
-    plt.xlabel("Technology", fontsize=12)
-    plt.ylabel("Frequency", fontsize=12)
-    plt.title("Frequency of Technologies in Job Descriptions", fontsize=14)
+    # Create a bar chart
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = ax.bar(technologies, frequencies, color="skyblue")
+    ax.set_xlabel("Technology", fontsize=12)
+    ax.set_ylabel("Frequency", fontsize=12)
+    ax.set_title("Frequency of Technologies in Job Descriptions", fontsize=14)
     plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
 
-    # Збереження діаграми у файл із зазначенням дати й часу
-    plt.savefig(str(diagram_file))
+    # Add frequency labels
+    ax.bar_label(bars, fmt='%d', padding=3, fontsize=10, color="black")
+
+    plt.savefig(diagram_file, dpi=300)
     plt.close()
 
-    print(f"Діаграма збережена у файл: {diagram_file}")
+    print(f"Diagram saved to: {diagram_file}")
