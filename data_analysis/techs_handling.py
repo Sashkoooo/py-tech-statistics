eef7397590823eb1b3a0c75a02c9a0a9972b82a8
Sashkoooo <o.denysenko@hotmail.com>
2025-02-04 14:32:43 +0200

    Moved arguments to function parameters, rename function

--- data_analysis/techs_handling.py
+++ data_analysis/techs_handling.py
@@ -2,8 +2,12 @@ from data_analysis.diagram import diagram
 from data_analysis.techs_counting import TechnologyCounting
 
 
-def techs_handling(file_name: str) -> None:
+def python_techs_handling(
+        source_file_name: str,
+        category: str = "Python",
+        site_name: str = "Dou.ua",
+    ) -> None:
     """Count technologies, save to csv and create diagram"""
-    techs = TechnologyCounting(file_name)
+    techs = TechnologyCounting(source_file_name)
     techs.save_to_csv()
-    diagram(techs.technology_counter)
+    diagram(techs.technology_counter, category, site_name)
