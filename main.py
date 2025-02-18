c445a248190b75453401181c042463b6d5b67ea3
Sashkoooo <o.denysenko@hotmail.com>
2025-02-18 19:30:33 +0200

    code refactoring

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import subprocess
 
-from data_analysis.techs_handling import techs_handling
+from data_analysis.data_handling import python_techs_handling
 from links_parser.urls_handling import get_dou_links
 
 
 if __name__ == "__main__":
 
     get_dou_links()
-    print("Links gathered successfully.")
+    print("Dou.ua links gathered successfully.")
 
     print("Starting Scrapy crawler...")
     try:
         subprocess.run(
-            "python -m scrapy crawl dou_spider -o data/source/dou.jsonl",
+            "python -m scrapy crawl dou_spider -O data/source/dou.jsonl",
             check=True
         )
         print("Scrapy crawler completed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error while running Scrapy: {e}")
 
-    techs_handling("dou.jsonl")
+    python_techs_handling("dou.jsonl")
+
